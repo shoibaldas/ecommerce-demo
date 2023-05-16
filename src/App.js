e9157896c360a2cd24d07eec58544cabdec9e751
Shoibal Das <your_email@example.com>
2023-05-16 14:50:20 +0600

    project structure intialized along with router dom

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/Routes';
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={routes}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
