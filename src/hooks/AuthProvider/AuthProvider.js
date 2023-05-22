c40f1b850ea1eb5a7890d194b9edd9086f0a86de
Shoibal Das <your_email@example.com>
2023-05-22 10:41:57 +0600

    state issue resolved in user context for private route issues

--- src/hooks/AuthProvider/AuthProvider.js
+++ src/hooks/AuthProvider/AuthProvider.js
@@ -2,33 +2,31 @@ import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const [loading, setLoading] = useState(true);
-    const [signIn, setSignIn] = useState(true);
+const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [signIn, setSignIn] = useState(true);
 
-    const user = localStorage.getItem("user") && JSON.parse(localStorage.getItem("user"));
-    
-    const logout = () => {
-        localStorage.removeItem("user");
+  const user =
+    localStorage.getItem("user") && JSON.parse(localStorage.getItem("user"));
+
+  const logout = () => {
+    localStorage.removeItem("user");
+  };
+
+  useEffect(() => {
+    if (user) {
+      setLoading(false);
+    } else {
+      setSignIn(true);
+      setLoading(false);
     }
+  }, [user, signIn]);
 
-    useEffect(() => {
-        if (user) {
-            setSignIn(false);
-            setLoading(false);
-        }
-        else {
-            setSignIn(false);
-        }
-
-    }, [user, signIn])
-
-
-    return (
-        <UserContext.Provider value={{user, logout, signIn, setSignIn, loading}}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ user, logout, signIn, setSignIn, loading }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export default AuthProvider;
